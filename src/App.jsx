fdd190746218e28a5b92faa391fa09a52ecfccac
👨‍💻 Sandaru Malith Lakshan <135944062+Sandaru376@users.noreply.github.com>
2026-02-21 00:08:09 +0530

    Update text in App.jsx for HMR test

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR TEST 2026
+          Edit <code>src/App.jsx</code> and save to test HMR TEST 2026 EEEEEEEE
         </p>
       </div>
       <p className="read-the-docs">
